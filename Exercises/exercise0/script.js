77f2386700c5bcb1113e42e79128f5b9d4fa44b9
Lilia <l_guirre@cda074ev5815x.local>
2019-09-10 16:25:06 -0400

    exercise0

---
+++ Exercises/exercise0/script.js
@@ -0,0 +1,93 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+createCanvas(500,500)
+//rectangles
+noStroke()
+rectMode(CENTER);
+background(61,224,213,);
+//rect 6
+fill(73, 178, 230,127)
+rect(250,250,400,400);
+//rect 5
+fill(73, 128, 230,127);
+rect(250,250,250,250);
+//rect 4
+fill(73, 89, 230,127)
+rect(250,250,150,150);
+//rect 3
+fill(94, 73, 230, 127)
+rect(250,250,100,100);
+//rect 2
+fill(75, 32, 186,127)
+rect(250,250,50,50);
+//rect 1
+fill(49, 13, 158,127)
+rect(250,250,20,20)
+//circles
+//smallest circle
+fill(110, 27, 103,170)
+ellipse(254,254,10,10)
+fill(125, 34, 118,170)
+ellipse(210,220,15,15)
+fill(138, 40, 130,170)
+ellipse(300,230,20,20)
+fill(150, 47, 142,170)
+ellipse(300,300,30,30)
+fill(163, 55, 154,170)
+ellipse(250,160,40,40)
+fill(179, 64, 169,170)
+ellipse(160,250,65,65)
+fill(189, 72, 179,170)
+ellipse(100,100,80,80)
+fill(217, 91, 206,170)
+ellipse(400,400,90,90)
+fill(201, 81, 191,170)
+ellipse(430,160,100,100)
+//biggest circle
+fill(232, 104, 221,170)
+ellipse(100, 500, 300,300)
+//mirrored circles with changes
+fill(125, 34, 118,170)
+ellipse(245,230,10,10)
+fill(138, 40, 130,170)
+ellipse(230,300,20,20)
+fill(163, 55, 154,170)
+ellipse(60,200,40,40)
+fill(179, 64, 169,170)
+ellipse(340,290,75,75)
+fill(201, 81, 191,170)
+ellipse(200,360,100,100)
+fill(232, 104, 221,170)
+ellipse(300,50,200,200)
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
