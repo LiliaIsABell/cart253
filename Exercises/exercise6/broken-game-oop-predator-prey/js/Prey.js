e902d45f6301478cfe9e928367bbe1a35af1271a
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2019-10-26 22:49:07 -0400

    Ex6: Fixed error in the move function
    The move function was incorrectly named "mover" which would not allow the preys to move, and so I renamed it move().

--- Exercises/exercise6/broken-game-oop-predator-prey/js/Prey.js
+++ Exercises/exercise6/broken-game-oop-predator-prey/js/Prey.js
@@ -5,7 +5,7 @@
 // the screen and be consumed by Predator objects.
 
 /////////////////////////
-// ~7 ERRORS IN HERE
+// ~6 ERRORS IN HERE
 /////////////////////////
 
  //////////////// FIXED
@@ -40,7 +40,9 @@
   //
   // Sets velocity based on the noise() function and the Prey's speed
   // Moves based on the resulting velocity and handles wrapping
-  mover() {
+
+  //////////////// FIXED
+  move() {
     // Set velocity via noise()
     this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
     this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
