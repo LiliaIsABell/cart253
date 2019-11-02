61310d4b05bc1cc1f835173c1a8c6379fe71f3bc
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2019-11-02 10:49:41 -0400

    P2: Created a new class called enemy
    This class creates an enemy that makee the game end when the game is over.

---
+++ Exercises/Project2/game-oop-predator-prey/js/Enemy.js
@@ -0,0 +1,20 @@
+// Enemy
+//
+// A class that represents an enemy that will move
+// on screen based on a noise() function. It can move around
+// the screen and be consumed by Predator objects but
+// if the Predator touches it, game over.
+
+class Enemy {
+
+
+
+
+
+
+
+
+
+
+
+}
