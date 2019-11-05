1a6581ad4fb2aafc20a77b731bc85660468226e9
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2019-11-05 14:46:09 -0500

    P2: Created Array for the Images used

--- Exercises/Project2/game-oop-predator-prey/js/script.js
+++ Exercises/Project2/game-oop-predator-prey/js/script.js
@@ -19,17 +19,8 @@ let marbles = [1, 2];
 // The power up
 let powerUp;
 
-// The background image
-let carpet;
-
-// The prey image
-let dustbunny;
-
-// The predator image
-let vaccum;
-
-// The enemy image
-let marble;
+// Array for Images used
+let images = ["carpet", "dustbunny", "vaccum", "marble"]
 
 // The CatMeow font
 let dusty;
@@ -44,13 +35,13 @@ let gameOver = false;
 function preload() {
 
   // Loading carpet image
-  carpet = loadImage("assets/images/carpet.jpg");
+  images[0] = loadImage("assets/images/carpet.jpg");
   // Loading dust bunny image
-  dustbunny = loadImage("assets/images/dustbunny.png");
+  images[1] = loadImage("assets/images/dustbunny.png");
   // Loading vaccum image
-  vaccum = loadImage("assets/images/vaccum.png");
+  images[2] = loadImage("assets/images/vaccum.png");
   // Loading marble image
-  marble = loadImage("assets/images/marble.png");
+  images[3] = loadImage("assets/images/marble.png");
   // Loading CatMeow font
   dusty = loadFont("assets/fonts/catMeow.ttf");
 }
@@ -61,14 +52,14 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // The predator has a variable for the image
-  tiger = new Predator(100, 100, 5, color(74, 39, 39), 60, vaccum, 0);
+  tiger = new Predator(100, 100, 5, color(74, 39, 39), 60, images[2], 0);
   // The preys have a variable for the image
-  dustBunnies[0] = new Prey(100, 100, 10, color(255, 100, 10), 60, dustbunny);
-  dustBunnies[1] = new Prey(100, 100, 8, color(255, 255, 255), 100, dustbunny);
-  dustBunnies[2] = new Prey(100, 100, 20, color(255, 255, 0), 40, dustbunny);
+  dustBunnies[0] = new Prey(100, 100, 10, color(255, 100, 10), 60, images[1]);
+  dustBunnies[1] = new Prey(100, 100, 8, color(255, 255, 255), 100, images[1]);
+  dustBunnies[2] = new Prey(100, 100, 20, color(255, 255, 0), 40, images[1]);
   // Setup for the enemies
-  marbles[0] = new Enemy(windowWidth / 2, 0, 5, 20, marble);
-  marbles[1] = new Enemy(windowWidth / 4, 0, 5, 20, marble);
+  marbles[0] = new Enemy(windowWidth / 2, 0, 5, 20, images[3]);
+  marbles[1] = new Enemy(windowWidth / 4, 0, 5, 20, images[3]);
   // Setup the power up
   powerUp = new Powerup(windowWidth, windowHeight / 2, 10, 40, color(3, 252, 94), color(3, 73, 252), 20);
 }
@@ -88,7 +79,7 @@ function draw() {
   // the game will start and the player can play
   else if (gameOver === false && playing === true) {
     // The background is an image of a carpet
-    background(carpet);
+    background(images[0]);
 
     // Handle input for the tiger
     tiger.handleInput();
@@ -147,7 +138,7 @@ function draw() {
 function titleScreen() {
   push();
   // The background is carpet
-  background(carpet);
+  background(images[0]);
   // Brown title
   fill(74, 39, 39);
   // The font will be the CatMeow font
@@ -172,14 +163,14 @@ function titleScreen() {
   // Instructions
   //
   // How to Move
-  image(vaccum, windowWidth / 40, windowHeight / 2.5, 300, 300);
+  image(images[2], windowWidth / 40, windowHeight / 2.5, 300, 300);
 
   textAlign(CENTER, TOP);
   textSize(20);
   text("Use the arrow keys \n\n to move your vaccum", windowWidth / 3.5, windowHeight / 1.7);
 
   // Warnings
-  image(marble, 1205, windowHeight / 2.5, 50, 50);
+  image(images[3], 1205, windowHeight / 2.5, 50, 50);
 
   textAlign(CENTER, TOP);
   textSize(20);
@@ -190,7 +181,7 @@ function titleScreen() {
 // Ending screen function
 function endScreen() {
 
-  background(carpet);
+  background(images[0]);
 
   textAlign(CENTER, TOP);
   fill(74, 39, 39);
