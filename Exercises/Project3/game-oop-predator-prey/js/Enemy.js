365a7446e0f55eb116148d7573f139688d875659
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2019-11-11 10:18:44 -0500

    P3: Started Project 3

---
+++ Exercises/Project3/game-oop-predator-prey/js/Enemy.js
@@ -0,0 +1,36 @@
+// Enemy
+//
+class Enemy {
+
+// Constructor
+//
+  constructor() {
+
+  }
+
+// Move
+//
+  move() {
+
+  }
+// Handle Game Over
+//
+  handleGameOver(predator) {
+
+    }
+
+
+// Reset
+//
+  reset() {
+
+  }
+
+  // Display
+  display() {
+
+
+  }
+
+
+}
