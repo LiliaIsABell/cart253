3c143f1f9ebe8e12844546c2be1f389f0cfc3cad
LiliaIsABell <54867489+LiliaIsABell@users.noreply.github.com>
2019-11-11 18:02:19 -0500

    P3: Setup Prey
    One prey has been setup and is moving based on time. It has a timer that counts if 5 seconds has passed, and, if yes, the prey will be placed elsewhere.

--- Exercises/Project3/game-oop-predator-prey/js/Prey.js
+++ Exercises/Project3/game-oop-predator-prey/js/Prey.js
@@ -4,29 +4,48 @@ class Prey {
 
   // Constructor
   //
-  constructor() {
-
-
-
-
+  constructor(x, y, radius, bodyColor, strokeColor) {
+    // Position
+    this.x = x;
+    this.y = y;
+    // Health
+    this.health = this.radius;
+    // Display properties
+    this.radius = radius;
+    // Colors
+    this.bodyColor = bodyColor;
+    this.strokeColor = strokeColor;
+    // Timer (Used for moving the prey)
+    this.startTime = millis();
+    this.timePast = 0;
+    this.timeInterval = 5000;
   }
 
   // Move
   //
   move() {
-
-
+    // The prey's timer will start when the prey is displayed
+this.timePast=millis()-this.startTime;
+  // When the prey has been put for 5 seconds,
+  // it's position will reset
+  if (this.timePast > this.timeInterval){
+      this.x = random(0, width);
+      this.y = random(0, height);
+      // This allows the timer to reset
+      this.startTime = millis();
+      this.timePast = 0;
+}
   }
 
   // Display
   //
   display() {
 
+    push();
+    fill(this.bodyColor);
+    stroke(this.strokeColor);
+    ellipse(this.x, this.y, this.radius*2, this.radius*2);
+    pop();
   }
 
-  // Reset
-  //
-  reset() {
-
-  }
 }
--- Exercises/Project3/game-oop-predator-prey/js/script.js
+++ Exercises/Project3/game-oop-predator-prey/js/script.js
@@ -5,19 +5,36 @@
 // The predator chases the prey using the arrow keys and consumes them.
 // The predator loses health over time, so must keep eating to survive.
 
+// THIS WILL BE THE GAMES TITLE
+// by Lilia Isabel Aguirre Lugo
+// This game has a predator that chases after ther preys, but there is also an
+// enemy that will incovenience the predator. The predator is moves using the
+// arrow keys, increases in speed when consuming a prey and slows down when
+// interacting with the ennemy.
+//******************************************************************************
 
 
+// Variable for prey
+let happybits;
 
 
 // Setup
 //
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  // Setup the prey
+  happybits = new Prey(300, 300, 20, color(255, 0, 0), color(0, 0, 255));
 
 }
 
 // Draw
 //
 function draw() {
+  // Background is dark purple
+  background(49, 0, 87);
+  // Move Prey
+  happybits.move();
+  // Display Prey
+  happybits.display();
 
 }
